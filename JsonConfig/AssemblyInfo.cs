a3cc80bdd3d20e314e24bdf3f49ef31803c9cf86
Timo Dörr <timo@latecrew.de>
2012-10-09 18:08:59 +0200

    Add MIT/X11 licensing text to each source file

--- JsonConfig/AssemblyInfo.cs
+++ JsonConfig/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("td")]
+[assembly: AssemblyCopyright("Timo Dörr")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
